fb2ecb1b3affed0abcb557202c579528d1c8f857
Pozzame <pozzame@gmail.com>
2024-12-03 23:19:34 +0100

    Ok

---
+++ src/Acme.BookStore.Web/Pages/v-table/v-table.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Acme.BookStore.Web.Pages.v_table
+{
+    public class v_tableModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
